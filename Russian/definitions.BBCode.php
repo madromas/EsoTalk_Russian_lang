f1efbf7d7e4ab12af471c285735e8ff38c0ffb47
Roman <64708726+madromas@users.noreply.github.com>
2022-02-13 19:54:14 -0500

    Add files via upload

---
+++ Russian/definitions.BBCode.php
@@ -0,0 +1,9 @@
+<?php
+
+	$definitions["Code"] = "Код";
+	$definitions["Image"] = "Изображение";
+	$definitions["Link"] = "Ссылка";
+	$definitions["Strike"] = "Зачёркнутый текст";
+	$definitions["Header"] = "Заголовок";
+	$definitions["Italic"] = "Наклонный текст";
+	$definitions["Bold"] = "Жирный текст";
